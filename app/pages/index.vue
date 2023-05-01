1179895286d90646cd9185a29776e5acba5e1fc2
Payal Raviya <87559461+Payal17122000@users.noreply.github.com>
2023-05-01 16:49:18 +0530

    Revert "feat: added comment and reply feature (#2)"
    This reverts commit f4779a4c39bb8c115b1391ab5cc20d4b8c32d8a2.

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,41 +1,15 @@
 <script setup>
-let message = ref('')
-let CommentStore = ref()
-
-let { data: comments } = useAsyncData('comment', () =>
-  $fetch('http://localhost:3004/comments')
-)
-CommentStore.value = comments.value
-function addData() {
-  if (message.value != '') {
-    const commentData = {
-      _id: '',
-      picture: '',
-      auther: '',
-      gender: '',
-      comment: message.value,
-      liked: true,
-      isActive: true,
-    }
-    CommentStore.value.push(commentData)
-    message.value = ''
-  }
-}
+/*Define Pinia store*/
+/*import { useUsersStore } from '~/store/users'
+const UsersStore = useUsersStore()
+console.log(UsersStore);*/
 </script>
 <template>
-  <div class="ms-5">
-    <div class="d-block">
-      <input v-model="message" type="text" name="comment" class="m-3" />
-      <button @click="addData()">Comment</button>
-    </div>
-    <div v-for="(item, index) in comments" :key="index">
-      <Comment
-        v-bind="{
-          comment: item.comment,
-          replies: item.comments,
-        }"
-      />
-    </div>
-  </div>
+  <div class="text-center homepage">Welcome to nuxt3</div>
 </template>
-<style scoped></style>
+<style scoped>
+.homepage {
+  margin-top: 400px;
+  font-size: 30px;
+}
+</style>
