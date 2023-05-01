f4779a4c39bb8c115b1391ab5cc20d4b8c32d8a2
Nilam Singh <123356326+nilam-singh@users.noreply.github.com>
2023-05-01 16:43:41 +0530

    feat: added comment and reply feature (#2)
    Co-authored-by: nilam-singh <nilam.singh@improwiesd.com>

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,41 @@
 <script setup>
-/*Define Pinia store*/
-/*import { useUsersStore } from '~/store/users'
-const UsersStore = useUsersStore()
-console.log(UsersStore);*/
+let message = ref('')
+let CommentStore = ref()
+
+let { data: comments } = useAsyncData('comment', () =>
+  $fetch('http://localhost:3004/comments')
+)
+CommentStore.value = comments.value
+function addData() {
+  if (message.value != '') {
+    const commentData = {
+      _id: '',
+      picture: '',
+      auther: '',
+      gender: '',
+      comment: message.value,
+      liked: true,
+      isActive: true,
+    }
+    CommentStore.value.push(commentData)
+    message.value = ''
+  }
+}
 </script>
 <template>
-  <div class="text-center homepage">Welcome to nuxt3</div>
+  <div class="ms-5">
+    <div class="d-block">
+      <input v-model="message" type="text" name="comment" class="m-3" />
+      <button @click="addData()">Comment</button>
+    </div>
+    <div v-for="(item, index) in comments" :key="index">
+      <Comment
+        v-bind="{
+          comment: item.comment,
+          replies: item.comments,
+        }"
+      />
+    </div>
+  </div>
 </template>
-<style scoped>
-.homepage {
-  margin-top: 400px;
-  font-size: 30px;
-}
-</style>
+<style scoped></style>
